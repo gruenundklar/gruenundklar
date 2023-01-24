2a358197da4800873576e9b778d6fcc9e45e1b11
kotrakesh <kotrakesh@gmail.com>
2023-01-24 16:34:09 +0100

    Update Project Information

---
+++ tt_web_gruenuklar_layout/model/sale_order.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, tools, _, SUPERUSER_ID
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    bearbeiter = fields.Selection([('Roland Grabenauer', 'Roland Grabenauer'),
+                                      ('Sascha Hotz', 'Sascha Hotz'),
+                                      ], string='Sachbearbeiter', default='Roland Grabenauer')
